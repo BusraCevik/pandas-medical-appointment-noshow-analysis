23b12f1d222b79d65a9b9f5412fbd12b0410f207
Busra Cevik <busrazcvik@outlook.com>
2026-01-15 18:58:15 +0300

    data_preparation.py done, clean.csv created.

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -4,7 +4,28 @@ import pandas as pd
 def prepare_data(input_path, output_path):
     df = pd.read_csv(input_path)
 
-    df.isnull().sum()
     print(df.isnull().sum())
+
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.lower()
+        .str.replace("-", "_")
+        .str.replace(" ", "_")
+    )
+
+    df["scheduledday"] = pd.to_datetime(df["scheduledday"])
+    df["appointmentday"] = pd.to_datetime(df["appointmentday"])
+
+    df = df[(df["age"] >= 0) & (df["age"] <= 110)]
+
+    print(df["age"].dtype)
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+
+    print("Processed data saved to:", output_path)
+
+
 
 
